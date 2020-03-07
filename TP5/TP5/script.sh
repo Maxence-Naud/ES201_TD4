ac21ed20f391c73be6dbb1a5b780c393857eea91
Sanchez diaz Kevin alexandro <sanchez-diaz@salle.ensta.fr>
2020-03-07 22:56:50 +0100

    exe

--- TP5/TP5/script.sh
+++ TP5/TP5/script.sh
@@ -6,7 +6,7 @@ do
 	for (( i=1; i <= k; i =i*2 ));
 	do
 		echo "${i} threads" >> result_exe_4.txt
-		$GEM5/build/ARM/gem5.fast $GEM5/configs/example/se.py -n ${i} -w $1 --caches --cpu-type=detailed -c test_omp -o "${i} 32" | grep -i "Done" >> result_exe_4.txt
+		$GEM5/build/ARM/gem5.fast $GEM5/configs/example/se.py -n ${i} -w $1 --caches --cpu-type=detailed -c test_omp -o "${i} ${k}" | grep -i "Done" >> result_exe_4.txt
 		cat ./m5out/stats.txt | grep -e "system.cpu.numCycles" -e "system.cpu0.numCycles" -e "system.cpu00.numCycles" >> result_exe_4.txt
 		cat ./m5out/stats.txt | grep -i "sim_inst" >> result_exe_4.txt
 	done
